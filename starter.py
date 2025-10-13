3b8387a09623ccad3854070fd70c872c88d2de6a
ArtemKucheruk <a.o.kucheruk@gmail.com>
2025-10-13 12:27:52 +0300

    add logger to the basic setup

--- starter.py
+++ starter.py
@@ -1,10 +1,12 @@
 import sys
 from windows import MainWindow
 from PyQt6.QtWidgets import QApplication
+from utils import logger
 
 
 def starter():
     app = QApplication(sys.argv)
+    logger.info("app was initialized")
     window = MainWindow()
     window.show()
     app.exec()
--- windows/main_window.py
+++ windows/main_window.py
@@ -1,6 +1,8 @@
 from PyQt6.QtWidgets import QMainWindow
+from utils import logger
 
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
+        logger.info("Main window was initialized")
