14f934f7428f23b1eb2df8623129120e6dc8ffd3
danyilispaniuk <danulispanyk@gmail.com>
2025-10-14 14:38:17 +0300

    file chooser fixed

--- starter.py
+++ starter.py
@@ -1,15 +1,21 @@
 import sys
+
 from windows.main_window_funcs import MenuFuncs
-from PyQt6.QtWidgets import QApplication, QDialog
+from PyQt6.QtWidgets import QApplication, QDialog, QPushButton, QFileDialog
 from PyQt6.uic import loadUi
 from utils import logger
 
 
 class InitWindow(QDialog):
+    def choose_file(self):
+        MenuFuncs.choose_file()
+
     def __init__(self):
-        super(InitWindow, self).__init__()
+        super().__init__()
         loadUi("ui.ui", self)
-        #self.btn_choose_file.clicked.connect(MenuFuncs.choose_file)
+        self.btn_choose_file = self.findChild(QPushButton, "btn_choose_file")
+        self.btn_choose_file.clicked.connect(self.choose_file)
+
 
 def starter():
     app = QApplication(sys.argv)
--- windows/main_window_funcs/menu_funcs.py
+++ windows/main_window_funcs/menu_funcs.py
@@ -3,42 +3,28 @@ import os
 import datetime
 
 
-class MenuFuncs(QDialog):
-    def choose_file(self):
-        file_path, _ = QFileDialog.getOpenFileName(self, "Choose a file", "", "All Files (*)",
-                                                   options=QFileDialog.Options())
-        # if file_path:
-        #     self.lb_adress.setText("File address: " + file_path)
-        #
-        #     file_size = round(os.path.getsize(file_path) / 1024, 2)
-        #     if file_size > 1024:
-        #         file_size = round((file_size / 1024), 2)
-        #         if file_size > 1024:
-        #             file_size = round((file_size / 1024), 2)
-        #             self.lb_size.setText("Size: " + str(file_size) + "GB")
-        #         else:
-        #             self.lb_size.setText("Size: " + str(file_size) + "MB")
-        #     else:
-        #         self.lb_size.setText("Size: " + str(file_size) + "KB")
-        #
-        #     file_type = str(os.path.splitext(file_path)[1])
-        #     self.lb_type.setText("Type: " + file_type)
-        #
-        #     file_name = os.path.basename(file_path)
-        #     self.lb_name.setText("Name: " + file_name)
-        #
-        #     file_create = os.stat(file_path).st_ctime
-        #     file_create = datetime.datetime.fromtimestamp(file_create)
-        #     formatted_date = file_create.strftime("%Y-%m-%d %H:%M:%S")
-        #     self.lb_date.setText(str("Created: " + formatted_date))
-        #
-        #     file_mod = os.stat(file_path).st_mtime
-        #     file_mod = datetime.datetime.fromtimestamp(file_mod)
-        #     formatted_mod = file_mod.strftime("%Y-%m-%d %H:%M:%S")
-        #     self.lb_date.setText(str("Modified: " + formatted_mod))
-        #
-        #     return file_path
-        # else:
-        #     print("No file selected")
+class MenuFuncs:
+    @staticmethod
+    def choose_file():
+        print("choose_file")
+        file_name, _ = QFileDialog.getOpenFileName(
+            parent=None,
+            caption="Choose an MP4 file",
+            directory="",
+            filter="MP4 Files (*.mp4)"
+        )
 
-        return
+        print(file_name)
+        # options = QFileDialog.Option.DontUseNativeDialog
+        # file_name, _ = QFileDialog.getOpenFileName(
+        #     None,
+        #     "Choose a file",
+        #     "",
+        #     "All Files (*)",
+        #     options=options
+        # )
+        # print("choose_file3")
+
+        # QFileDialog.getOpenFileName(parent=None, caption='', directory='', filter='', options=QFileDialog.Options())
+
+
