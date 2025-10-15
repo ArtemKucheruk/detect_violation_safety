916bb650a7f7aa1d8b3a11e4b3ae5e6af208997b
ArtemKucheruk <a.o.kucheruk@gmail.com>
2025-10-15 14:07:19 +0300

    add a class to predict the result based on the llm

---
+++ yolo/predict.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+
+
+class LlmAnalyze:
+    def __init__(self) -> None:
+        self.model = YOLO("")  # change to the path of the model
+
+    def predict(self, video_file_name):
+        result = self.model.predict(
+            source=video_file_name,
+            project="video_results",
+            name="analysis_run",
+            save=True,
+            conf=0.25,
+            save_txt=True,
+            save_conf=True,
+            exist_ok=True,
+        )
+        return result
+
+
+llm_analyze = LlmAnalyze()
